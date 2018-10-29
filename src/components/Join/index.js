380139dcd063be24be2a549620ae69ffc37f026f
Sergey Bakhar <b113.bahar@gmail.com>
2018-10-29 16:05:03 +0200

    Add styles to the "Join us" component

--- src/components/Join/index.js
+++ src/components/Join/index.js
@@ -1,10 +1,23 @@
 import React, { Component } from 'react';
+import styles from './join.module.css';
 
 class Join extends Component {
   render() {
     return (
-      <div className="Join">
-          Join
+      <div className={styles.joinUs}>
+        <form action="#" name="joinUsForm" className={styles.joinUsForm} method="post">
+
+          <label htmlFor="username" className={styles.joinUsForm__usernameLabel}>Username</label>
+          <input type="text" name="username" className={styles.joinUsForm__usernameInput} />
+
+          <label htmlFor="password" className={styles.joinUsForm__passwordLabel}>Password</label>
+          <input type="password" name="password" className={styles.joinUsForm__passwordInput} />
+
+          <label htmlFor="password_confirm" className={styles.joinUsForm__passwordConfirmLabel}>Confirm password</label>
+          <input type="password" name="password_confirm" className={styles.joinUsForm__passwordConfirmInput} />
+
+          <input type="submit" value="Join us" className={styles.joinUsForm__submitBtn} />
+        </form>
       </div>
     );
   }
--- src/components/Sign/index.js
+++ src/components/Sign/index.js
@@ -8,10 +8,13 @@ class Sign extends Component {
         <form action="#" name="signInForm" className={styles.signInForm} method="post">
           <label htmlFor="username" className={styles.signInForm__usernameLabel}>Username</label>
           <input type="text" name="username" className={styles.signInForm__usernameInput} />
+          
           <label htmlFor="password" className={styles.signInForm__passwordLabel}>Password</label>
           <input type="password" name="password" className={styles.signInForm__passwordInput} />
+          
           <input type="checkbox" name="keepsigned" id="keepsigned" className={styles.signInForm__keepsignedInput} />
           <label htmlFor="keepsigned" className={styles.signInForm__keepsignedLabel}>Keep me signed in</label>
+          
           <input type="submit" value="Sign in" className={styles.signInForm__submitBtn} />
         </form>
         <a href="#" className={styles.signIn__recoverLinkPass}>Forgot password</a>
