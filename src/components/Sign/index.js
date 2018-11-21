e708c346e8db762d3cd9531d95d2a9af332ab9dd
Sergey Bakhar <b113.bahar@gmail.com>
2018-11-21 13:49:51 +0200

    Add validation to email input field in Sign module

--- src/components/Sign/index.js
+++ src/components/Sign/index.js
@@ -2,20 +2,44 @@ import React, { Component } from 'react';
 import styles from './sign.module.css';
 
 class Sign extends Component {
+
+  state = {
+    errorEmail: false,
+    btnDisabled: true,
+  }
+
+  emailValidation = e => {
+    const re = /^[a-zA-Z0-9_\-.]+@[a-zA-Z0-9\-]+\.[a-zA-Z0-9\-.]+$/;
+    const { value } = e.target;
+
+    if (re.test(value)) {
+      this.setState({ 
+        errorEmail: false,
+        btnDisabled: false,
+       })
+    } else {
+      this.setState({ 
+        errorEmail: true,
+        btnDisabled: true,
+       })
+    }
+  }
+
   render() {
+    const { errorEmail, btnDisabled } = this.state;
     return (
       <div className={styles.signIn}>
         <form action="#" name="signInForm" className={styles.signInForm} method="post">
-          <label htmlFor="username" className={styles.signInForm__usernameLabel}>Username</label>
-          <input type="text" name="username" className={styles.signInForm__usernameInput} />
-          
+          <label htmlFor="username" className={styles.signInForm__usernameLabel}>E-mail</label>
+          <input type="text" name="username" placeholder="Your e-mail" className={ errorEmail ? `${styles.signInForm__usernameInput_error} ${styles.signInForm__usernameInput}` : styles.signInForm__usernameInput} onChange={this.emailValidation} />
+
           <label htmlFor="password" className={styles.signInForm__passwordLabel}>Password</label>
-          <input type="password" name="password" className={styles.signInForm__passwordInput} />
-          
+          <input type="password" name="password" placeholder="Your password" className={styles.signInForm__passwordInput} />
+
           <input type="checkbox" name="keepsigned" id="keepsigned" className={styles.signInForm__keepsignedInput} />
           <label htmlFor="keepsigned" className={styles.signInForm__keepsignedLabel}>Keep me signed in</label>
-          
-          <input type="submit" value="Sign in" className={styles.signInForm__submitBtn} />
+
+          <input type="submit" disabled={btnDisabled} value="Sign in" className={ btnDisabled ? `${styles.signInForm__submitBtn_disabled} ${styles.signInForm__submitBtn}` : styles.signInForm__submitBtn} />
         </form>
         <a href="#" className={styles.signIn__recoverLinkPass}>Forgot password</a>
       </div>
